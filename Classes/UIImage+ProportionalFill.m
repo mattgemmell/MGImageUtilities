6d4057240710e5b8b2297faa618458bc7d3d4105
Nathan de Vries <nathan@atnan.com>
2011-07-23 15:00:01 +1000

    Use -[UIScreen scale] to determine whether or not to use the UIGraphicsBeginImageContextWithOptions API.

--- Classes/UIImage+ProportionalFill.m
+++ Classes/UIImage+ProportionalFill.m
@@ -14,92 +14,101 @@
 - (UIImage *)imageToFitSize:(CGSize)fitSize method:(MGImageResizingMethod)resizeMethod
 {
 	float imageScaleFactor = 1.0;
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 40000
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_4_0
 	if ([self respondsToSelector:@selector(scale)]) {
 		imageScaleFactor = [self scale];
 	}
 #endif
 	
-    float sourceWidth = [self size].width * imageScaleFactor;
-    float sourceHeight = [self size].height * imageScaleFactor;
-    float targetWidth = fitSize.width;
-    float targetHeight = fitSize.height;
-    BOOL cropping = !(resizeMethod == MGImageResizeScale);
+	float sourceWidth = [self size].width * imageScaleFactor;
+	float sourceHeight = [self size].height * imageScaleFactor;
+	float targetWidth = fitSize.width;
+	float targetHeight = fitSize.height;
+	BOOL cropping = !(resizeMethod == MGImageResizeScale);
 	
-    // Calculate aspect ratios
-    float sourceRatio = sourceWidth / sourceHeight;
-    float targetRatio = targetWidth / targetHeight;
-    
-    // Determine what side of the source image to use for proportional scaling
-    BOOL scaleWidth = (sourceRatio <= targetRatio);
-    // Deal with the case of just scaling proportionally to fit, without cropping
-    scaleWidth = (cropping) ? scaleWidth : !scaleWidth;
-    
-    // Proportionally scale source image
-    float scalingFactor, scaledWidth, scaledHeight;
-    if (scaleWidth) {
-        scalingFactor = 1.0 / sourceRatio;
-        scaledWidth = targetWidth;
-        scaledHeight = round(targetWidth * scalingFactor);
-    } else {
-        scalingFactor = sourceRatio;
-        scaledWidth = round(targetHeight * scalingFactor);
-        scaledHeight = targetHeight;
-    }
-    float scaleFactor = scaledHeight / sourceHeight;
-    
-    // Calculate compositing rectangles
-    CGRect sourceRect, destRect;
-    if (cropping) {
-        destRect = CGRectMake(0, 0, targetWidth, targetHeight);
-        float destX, destY;
-        if (resizeMethod == MGImageResizeCrop) {
-            // Crop center
-            destX = round((scaledWidth - targetWidth) / 2.0);
-            destY = round((scaledHeight - targetHeight) / 2.0);
-        } else if (resizeMethod == MGImageResizeCropStart) {
-            // Crop top or left (prefer top)
-            if (scaleWidth) {
+	// Calculate aspect ratios
+	float sourceRatio = sourceWidth / sourceHeight;
+	float targetRatio = targetWidth / targetHeight;
+	
+	// Determine what side of the source image to use for proportional scaling
+	BOOL scaleWidth = (sourceRatio <= targetRatio);
+	// Deal with the case of just scaling proportionally to fit, without cropping
+	scaleWidth = (cropping) ? scaleWidth : !scaleWidth;
+	
+	// Proportionally scale source image
+	float scalingFactor, scaledWidth, scaledHeight;
+	if (scaleWidth) {
+		scalingFactor = 1.0 / sourceRatio;
+		scaledWidth = targetWidth;
+		scaledHeight = round(targetWidth * scalingFactor);
+	} else {
+		scalingFactor = sourceRatio;
+		scaledWidth = round(targetHeight * scalingFactor);
+		scaledHeight = targetHeight;
+	}
+	float scaleFactor = scaledHeight / sourceHeight;
+	
+	// Calculate compositing rectangles
+	CGRect sourceRect, destRect;
+	if (cropping) {
+		destRect = CGRectMake(0, 0, targetWidth, targetHeight);
+		float destX, destY;
+		if (resizeMethod == MGImageResizeCrop) {
+			// Crop center
+			destX = round((scaledWidth - targetWidth) / 2.0);
+			destY = round((scaledHeight - targetHeight) / 2.0);
+		} else if (resizeMethod == MGImageResizeCropStart) {
+			// Crop top or left (prefer top)
+			if (scaleWidth) {
 				// Crop top
 				destX = 0.0;
 				destY = 0.0;
-            } else {
+			} else {
 				// Crop left
-                destX = 0.0;
+				destX = 0.0;
 				destY = round((scaledHeight - targetHeight) / 2.0);
-            }
-        } else if (resizeMethod == MGImageResizeCropEnd) {
-            // Crop bottom or right
-            if (scaleWidth) {
+			}
+		} else if (resizeMethod == MGImageResizeCropEnd) {
+			// Crop bottom or right
+			if (scaleWidth) {
 				// Crop bottom
 				destX = round((scaledWidth - targetWidth) / 2.0);
 				destY = round(scaledHeight - targetHeight);
-            } else {
+			} else {
 				// Crop right
 				destX = round(scaledWidth - targetWidth);
 				destY = round((scaledHeight - targetHeight) / 2.0);
-            }
-        }
-        sourceRect = CGRectMake(destX / scaleFactor, destY / scaleFactor, 
-                                targetWidth / scaleFactor, targetHeight / scaleFactor);
-    } else {
-        sourceRect = CGRectMake(0, 0, sourceWidth, sourceHeight);
-        destRect = CGRectMake(0, 0, scaledWidth, scaledHeight);
-    }
-    
-    // Create appropriately modified image.
-	UIImage *image = nil;
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 40000
-	if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 4.0) {
-		UIGraphicsBeginImageContextWithOptions(destRect.size, NO, 0.0); // 0.0 for scale means "correct scale for device's main screen".
-		CGImageRef sourceImg = CGImageCreateWithImageInRect([self CGImage], sourceRect); // cropping happens here.
-		image = [UIImage imageWithCGImage:sourceImg scale:0.0 orientation:self.imageOrientation]; // create cropped UIImage.
-		[image drawInRect:destRect]; // the actual scaling happens here, and orientation is taken care of automatically.
-		CGImageRelease(sourceImg);
-		image = UIGraphicsGetImageFromCurrentImageContext();
-		UIGraphicsEndImageContext();
+			}
+		}
+		sourceRect = CGRectMake(destX / scaleFactor, destY / scaleFactor, 
+								targetWidth / scaleFactor, targetHeight / scaleFactor);
+	} else {
+		sourceRect = CGRectMake(0, 0, sourceWidth, sourceHeight);
+		destRect = CGRectMake(0, 0, scaledWidth, scaledHeight);
 	}
+	
+	// Create appropriately modified image.
+	UIImage *image = nil;
+	
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_4_0
+	CGImageRef sourceImg = nil;
+	if ([UIScreen instancesRespondToSelector:@selector(scale)]) {
+		UIGraphicsBeginImageContextWithOptions(destRect.size, NO, 0.f); // 0.f for scale means "scale for device's main screen".
+		sourceImg = CGImageCreateWithImageInRect([self CGImage], sourceRect); // cropping happens here.
+		image = [UIImage imageWithCGImage:sourceImg scale:0.0 orientation:self.imageOrientation]; // create cropped UIImage.
+		
+	} else {
+		UIGraphicsBeginImageContext(destRect.size);
+		sourceImg = CGImageCreateWithImageInRect([self CGImage], sourceRect); // cropping happens here.
+		image = [UIImage imageWithCGImage:sourceImg]; // create cropped UIImage.
+	}
+	
+	CGImageRelease(sourceImg);
+	[image drawInRect:destRect]; // the actual scaling happens here, and orientation is taken care of automatically.
+	image = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
 #endif
+	
 	if (!image) {
 		// Try older method.
 		CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
@@ -115,19 +124,19 @@
 		CGImageRelease(finalImage);
 	}
 	
-    return image;
+	return image;
 }
 
 
 - (UIImage *)imageCroppedToFitSize:(CGSize)fitSize
 {
-    return [self imageToFitSize:fitSize method:MGImageResizeCrop];
+	return [self imageToFitSize:fitSize method:MGImageResizeCrop];
 }
 
 
 - (UIImage *)imageScaledToFitSize:(CGSize)fitSize
 {
-    return [self imageToFitSize:fitSize method:MGImageResizeScale];
+	return [self imageToFitSize:fitSize method:MGImageResizeScale];
 }
 
 
